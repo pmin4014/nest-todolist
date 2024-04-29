49a406ecef204a121721445b14d006b37ccb3635
박민혁 <mhp4004@gmail.com>
2024-04-30 01:16:15 +0900

    파일 초기화

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -7,28 +7,4 @@ import { UpdateBoardDto } from './dto/update-board.dto';
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
-  @Post()
-  create(@Body() createBoardDto: CreateBoardDto) {
-    return this.boardService.create(createBoardDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.boardService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.boardService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
-    return this.boardService.update(+id, updateBoardDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.boardService.remove(+id);
-  }
 }
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -7,20 +7,5 @@ export class BoardService {
   create(createBoardDto: CreateBoardDto) {
     return 'This action adds a new board';
   }
-
-  findAll() {
-    return `This action returns all board`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} board`;
-  }
-
-  update(id: number, updateBoardDto: UpdateBoardDto) {
-    return `This action updates a #${id} board`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} board`;
-  }
+  
 }
