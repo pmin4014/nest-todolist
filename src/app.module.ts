fdcd801602138f49ec1b78ba523aedb965807d70
박민혁 <mhp4004@gmail.com>
2024-04-30 02:27:19 +0900

    typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './typeORM.config';
 
 @Module({
-  imports: [BoardModule],
+  imports: [
+    BoardModule,
+    TypeOrmModule.forRoot(typeORMConfig)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -3,11 +3,15 @@ import { BoardService } from './board.service';
 import { BoardController } from './board.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './entities/board.entity';
+import { BoardRepository } from './board.repository';
 
 @Module({
   //typeorm이 사용할 엔티티 클래스 지정
   imports: [TypeOrmModule.forFeature([Board])],
   controllers: [BoardController],
-  providers: [BoardService],
+  providers: [
+    BoardService,
+    BoardRepository
+  ],
 })
 export class BoardModule {}
