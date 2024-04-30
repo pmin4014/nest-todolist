6df8cd3d3c78d5c25e00c55e78b21a8753c1bfdb
박민혁 <mhp4004@gmail.com>
2024-04-30 22:34:24 +0900

    로그인을 위한 auth 모듈 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeORM.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     BoardModule,
-    TypeOrmModule.forRoot(typeORMConfig)
+    TypeOrmModule.forRoot(typeORMConfig),
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
