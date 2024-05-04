9215a794a0a0b29278db2e2c4d338dcd7a446a93
박민혁 <mhp4004@gmail.com>
2024-05-04 23:05:53 +0900

    auth모듈에 passport, jwt모듈 등록

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,23 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import * as config from 'config'
+import { PassportModule } from '@nestjs/passport';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: jwtConfig.secret,
+      signOptions:{
+        expiresIn: jwtConfig.expiresIn
+      }
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository]
 })
