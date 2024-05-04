682574e1034e9ec7e033099a8535414e42d0b2c0
박민혁 <mhp4004@gmail.com>
2024-05-04 19:31:07 +0900

    repository 내용 작성, 회원가입 기능

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './auth-credential.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,8 @@ export class AuthService {
         @InjectRepository(UserRepository)
         private userRepository: UserRepository
     ){}
+
+    signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.createUser(authCredentialsDto);
+    }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { DataSource, Repository } from "typeorm";
 import { User } from "./entities/user.entity";
+import { AuthCredentialsDto } from "./auth-credential.dto";
 
 @Injectable()
 export class UserRepository extends Repository<User>{
@@ -9,5 +10,12 @@ export class UserRepository extends Repository<User>{
         //데이터베이스 작업을 하기위해 추가
         super(User, dataSource.createEntityManager());
         }
-        
+
+        async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+            const { username, password } = authCredentialsDto;
+            const user = this.create({ username, password });
+
+            await this.save(user);
+        }
+
 }
