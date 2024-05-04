c313904b4bc938ea7082d735c8364dbf0aff3057
박민혁 <mhp4004@gmail.com>
2024-05-04 19:46:37 +0900

    유효성 검사부분 추가

--- src/auth/auth-credential.dto.ts
+++ src/auth/auth-credential.dto.ts
@@ -1,11 +1,17 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDto {
     @IsNotEmpty()
     @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
 
     @IsNotEmpty()
     @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    //영어랑 숫자만 사용가능한 유효성 체크, 에러 생길때 메세지 전달
+    @Matches(/^[a-zA-Z0-9]*$/, {message:'password only accepts english and number'})
     password: string;
 }
